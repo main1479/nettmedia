34d7811f3d421ea7fcb833fcdb0967f10872661c
main1479 <m.main2402@gmail.com>
2021-10-23 20:00:46 +0600

    profile menu added

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ const floating = document.querySelector('.floating');
 const floatingLinks = document.querySelectorAll('.floating__link');
 const floatingTriggers = document.querySelectorAll('.floating__trigger');
 const floatingOverlay = document.querySelectorAll('.floating-overlay');
+const profile = document.querySelector('.profile');
 
 floatingLinks.forEach((link, i) => {
 	link.style.setProperty('--delay', `0.${i + 1}s`);
@@ -11,4 +12,15 @@ floatingTriggers.forEach((btn) => {
 	btn.addEventListener('click', function () {
 		floating.classList.toggle('active');
 	});
+});
+
+profile.addEventListener('click', (e) => {
+	const body = profile.querySelector('.profile__body');
+	profile.classList.toggle('active');
+	if (body.style.maxHeight) {
+		body.style.maxHeight = null;
+	} else {
+		body.style.maxHeight = body.scrollHeight + 5 + 'px';
+	}
+	if (e.target.closest('.profile__overlay')) body.style.maxHeight = null;
 });
